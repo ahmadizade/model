6e821a19fa435f11895c766f4c3d3d3123287398
Ali <a.rahimi.ha@gmail.com>
2019-01-08 15:26:19 +0330

    fix admin namespaces

--- src/Controller/Admin/Pages/PagesController.php
+++ src/Controller/Admin/Pages/PagesController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\Admin;
+namespace App\Controller\Admin\Pages;
 
 use App\Entity\Pages;
 use App\Service\ApiHelper;
--- src/Controller/Admin/Users/UsersController.php
+++ src/Controller/Admin/Users/UsersController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\Admin;
+namespace App\Controller\Admin\Users;
 
 use App\Service\DataBase;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
