20b0dc78983c2c1c7ac33c941580c2611ae1a9dd
Ali <a.rahimi.ha@gmail.com>
2019-03-10 14:09:02 +0330

    add helper service (route)

---
+++ src/Service/Helper.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class Helper extends Controller
+{
+
+    private $dataBase;
+    protected $container;
+
+    public function __construct(DataBase $dataBase, ContainerInterface $container)
+    {
+        $this->container = $container;
+        $this->dataBase = $dataBase;
+    }
+
+   public function routeAlias($routes = []){
+        $routeAlias = [
+          'site_homepage' => 'homepage'
+        ];
+        return $routeAlias;
+
+/*        $return = [];
+
+        foreach ($routeAlias as $key => $alias){
+            if(isset($routes[$key]))
+                $return[$key] = $alias;
+        }
+
+        foreach ($routes as $key => $route){
+
+        }*/
+   }
+
+}
